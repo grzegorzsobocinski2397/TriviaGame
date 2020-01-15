6ecc201f3b2902cd0e16df633791e66f1dd13126
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-01-15 17:24:57 +0100

    Create Base Value Converter

---
+++ TriviaGame/TriviaGame/Converters/BaseValueConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TriviaGame.Converters
+{
+    /// <summary>
+    /// Base class for converters that do convert values to specific XAML objects/properties.
+    /// </summary>
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+         where T : class, new()
+    {
+        #region Private Members
+
+        /// <summary>
+        /// A single static instance of this value converter
+        /// </summary>
+
+        private static T converter = null;
+
+        #endregion Private Members
+
+        #region Public Properties
+
+        /// <summary>
+        /// Provides a static instance of the value converter
+        /// </summary>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return converter ?? (converter = new T());
+        }
+
+        #endregion Public Properties
+
+        #region Value converter methods
+
+        /// <summary>
+        /// The method to convert one type to WPF specific one.
+        /// </summary>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        /// <summary>
+        /// The method to convert a value back from WPF specific one.
+        /// </summary>
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        #endregion Value converter methods
+    }
+}
