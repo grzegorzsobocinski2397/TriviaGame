85f4256baf1e2f8e432cb43d0ff396f39e2965da
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-01-15 18:26:03 +0100

    Bind Highscores Page to view model

--- TriviaGame/TriviaGame/Pages/HighscoresPage.xaml.cs
+++ TriviaGame/TriviaGame/Pages/HighscoresPage.xaml.cs
@@ -10,6 +10,8 @@ namespace TriviaGame.Pages
         public HighscoresPage()
         {
             InitializeComponent();
+
+            DataContext = new HighscoresViewModel();
         }
     }
 }
---
+++ TriviaGame/TriviaGame/ViewModels/HighscoresViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using TriviaGame.ViewModels.Base;
+
+namespace TriviaGame
+{
+    internal class HighscoresViewModel : BaseViewModel
+    {
+        #region Commands
+
+        /// <summary>
+        /// Moves the user to <see cref="ApplicationPage.FinalScore"/> page.
+        /// </summary>
+        public ICommand ReturnCommand { get; set; }
+
+        #endregion Commands
+
+        #region Constructor
+
+        public HighscoresViewModel()
+        {
+            ReturnCommand = new RelayCommand(() => ChangePage(ApplicationPage.FinalScore));
+        }
+
+        #endregion Constructor
+    }
+}
