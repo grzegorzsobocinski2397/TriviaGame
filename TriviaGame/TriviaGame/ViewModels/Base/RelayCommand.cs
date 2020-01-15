52988dbca37066e46ba044abdd1aeb5be5d7e960
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-01-15 17:32:30 +0100

    Create Base Command

---
+++ TriviaGame/TriviaGame/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TriviaGame.ViewModels.Base
+{
+    /// <summary>
+    /// Base Command that implements <see cref="ICommand"/> for correct functionality.
+    /// </summary>
+    internal class RelayCommand : ICommand
+    {
+        /// <summary>
+        /// Action to run
+        /// </summary>
+        private readonly Action action;
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        /// <summary>
+        /// Command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
