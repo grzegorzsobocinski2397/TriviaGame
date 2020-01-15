b7423813c5ea564c8089e1f936072ca346f98eea
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-01-15 17:27:39 +0100

    Create ApplicationPage to WPF page converter

---
+++ TriviaGame/TriviaGame/Converters/ApplicationPageValueConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using TriviaGame.Pages;
+
+namespace TriviaGame.Converters
+{
+    /// <summary>
+    /// Converts the ENUM values to XAML pages.
+    /// </summary>
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        /// <summary>
+        /// Converts from <see cref="ApplicationPage"/> to WPF Page.
+        /// </summary>
+        /// <param name="value">This is the <see cref="ApplicationPage"/> enum.</param>
+        /// <returns>WPF page.</returns>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.First:
+                    return new FirstPage();
+
+                case ApplicationPage.Game:
+                    return new GamePage();
+
+                case ApplicationPage.FinalScore:
+                    return new FinalScorePage();
+
+                case ApplicationPage.Highscores:
+                    return new HighscoresPage();
+
+                case ApplicationPage.Results:
+                    return new ResultsPage();
+
+                case ApplicationPage.User:
+                    return new UserPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            };
+        }
+
+        /// <summary>
+        /// Backwards convertion which is not implemented.
+        /// </summary>
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TriviaGame/TriviaGame/Models/ApplicationPage.cs
@@ -0,0 +1,34 @@
+﻿namespace TriviaGame
+{
+    /// <summary>
+    /// Contains possible pages in the application.
+    /// </summary>
+    public enum ApplicationPage
+    {
+        None,
+        /// <summary>
+        /// First page that loads. Contains starting option.
+        /// </summary>
+        First,
+        /// <summary>
+        /// Page with user's name input element.
+        /// </summary>
+        User,
+        /// <summary>
+        /// Page with currently active question.
+        /// </summary>
+        Game,
+        /// <summary>
+        /// Page with user's final output.
+        /// </summary>
+        FinalScore,
+        /// <summary>
+        /// Page with summary of user's results.
+        /// </summary>
+        Results,
+        /// <summary>
+        /// Page with current scores. 
+        /// </summary>
+        Highscores
+    }
+}
