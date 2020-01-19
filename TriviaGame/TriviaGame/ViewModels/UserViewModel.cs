7283dea12d885cc4c4edc332f7ef53cb851f4cd5
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-01-19 14:56:16 +0100

    Demand from the user to set a name before starting the game

--- TriviaGame/TriviaGame/ViewModels/UserViewModel.cs
+++ TriviaGame/TriviaGame/ViewModels/UserViewModel.cs
@@ -5,6 +5,20 @@ namespace TriviaGame
 {
     internal class UserViewModel : BaseViewModel
     {
+        #region Public Properties
+
+        /// <summary>
+        /// Value of the textbox.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Text that informs user about serialized questions.
+        /// </summary>
+        public string InformationText { get; set; }
+
+        #endregion Public Properties
+
         #region Commands
 
         /// <summary>
@@ -24,9 +38,28 @@ namespace TriviaGame
         public UserViewModel()
         {
             ReturnCommand = new RelayCommand(() => ChangePage(ApplicationPage.First));
-            StartCommand = new RelayCommand(() => ChangePage(ApplicationPage.Game));
+            StartCommand = new RelayCommand(() => StartGame());
         }
 
         #endregion Constructor
+
+        #region Private Methods
+
+        /// <summary>
+        /// Try starting the game if the <see cref="UserName"/> isn't empty.
+        /// </summary>
+        private void StartGame()
+        {
+            if (string.IsNullOrEmpty(UserName))
+            {
+                InformationText = "You must set your name!";
+            }
+            else
+            {
+                ChangePage(ApplicationPage.Game);
+            }
+        }
+
+        #endregion Private Methods
     }
 }
