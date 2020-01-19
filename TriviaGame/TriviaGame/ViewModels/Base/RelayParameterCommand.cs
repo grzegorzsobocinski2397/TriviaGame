6c2712862f01d50997dbbb3ab6240df06d6d4fa7
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-01-19 16:47:55 +0100

    Add Base Relay Parameter Command

---
+++ TriviaGame/TriviaGame/ViewModels/Base/RelayParameterCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TriviaGame
+{
+    internal class RelayParameterCommand : ICommand {
+        #region Private Members
+
+        /// <summary>
+        /// The action to run
+        /// </summary>
+        private Action<object> action;
+
+        #endregion Private Members
+
+        #region Public Events
+
+        /// <summary>
+        /// The event thats fired when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion Public Events
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constrcutor
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayParameterCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        #endregion Constructor
+
+        #region Command Methods
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the commands action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        #endregion Command Methods
+    }
+}
+}
